7475e45b136da95c7ab6135db4f9eb81ddbcdd94
Vector Le <vectorle@protonmail.com>
2020-04-16 14:47:01 -0700

    removed circle crop on images

--- app/src/main/java/com/example/snrproject/ListPics.kt
+++ app/src/main/java/com/example/snrproject/ListPics.kt
@@ -19,25 +19,21 @@ class ListPics : AppCompatActivity() {
         {
             crossfade(enable = true)
             crossfade(3000)
-            transformations(CircleCropTransformation())
         }
         imageView2.load("https://images.unsplash.com/photo-1558981806-ec527fa84c39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60")
         {
             crossfade(enable = true)
             crossfade(3000)
-            transformations(CircleCropTransformation())
         }
         imageView4.load("https://images.unsplash.com/photo-1582648440777-95268093ebf7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60")
         {
             crossfade(enable = true)
             crossfade(3000)
-            transformations(CircleCropTransformation())
         }
         imageView5.load("https://www.nerd-age.com/wp-content/uploads/2012/12/Jojos-Bizarre-Adventure-Joseph-in-Drag.jpg")
         {
             crossfade(enable = true)
             crossfade(3000)
-            transformations(CircleCropTransformation())
         }
     }
 
