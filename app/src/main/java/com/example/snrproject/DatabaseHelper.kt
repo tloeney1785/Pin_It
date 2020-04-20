8658364267b24612d709c9940f80debe0f1948d4
Vector Le <vectorle@protonmail.com>
2020-04-19 19:17:18 -0700

    removed unused import

--- app/src/main/java/com/example/snrproject/DatabaseHelper.kt
+++ app/src/main/java/com/example/snrproject/DatabaseHelper.kt
@@ -2,7 +2,6 @@ package com.example.snrproject
 
 import android.content.ContentValues
 import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
