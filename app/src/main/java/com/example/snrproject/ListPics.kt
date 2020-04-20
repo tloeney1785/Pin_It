d1c51f458d1798c7d7f27778e20456b7851c139c
MattAko <mattako66@gmail.com>
2020-04-20 15:40:38 -0700

    Added navigation buttons and username for images

--- app/src/main/java/com/example/snrproject/ListPics.kt
+++ app/src/main/java/com/example/snrproject/ListPics.kt
@@ -1,19 +1,14 @@
 package com.example.snrproject
 
 import android.os.Bundle
-import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.*
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.*
 import coil.api.load
 import kotlinx.android.synthetic.main.activity_list_pics.*
 import android.view.Gravity
-import android.R.attr.gravity
+import android.content.Intent
 import android.widget.LinearLayout
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
 
 
 class ListPics : AppCompatActivity() {
@@ -30,14 +25,30 @@ class ListPics : AppCompatActivity() {
         val res = dbHelper.allData
         for (i in res.indices) {
             val image = ImageView(this)
+            val user = TextView(this)
 
             // use layoutParams to set image styling
             val layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
             layoutParams.gravity = Gravity.CENTER
             image.setLayoutParams(layoutParams)
+            user.setLayoutParams(layoutParams)
 
             image.load(res[i].userURL)
+            user.text = res[i].userName
             layout.addView(image)
+            layout.addView(user)
+        }
+
+        btnProfile.setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+        btnHome.setOnClickListener {
+            startActivity(Intent(this, ListPics::class.java))
+        }
+
+        btnUpload.setOnClickListener {
+            startActivity((Intent(this, MainActivity::class.java)))
         }
     }
 }
