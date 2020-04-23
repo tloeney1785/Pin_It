c2cc5dad71a8b30478f114c5e80427840b3dea34
Tony Lee <tloeney1785@gmail.com>
2020-04-22 19:30:53 -0700

    added login authorization with LOCAL db

--- app/src/main/java/com/example/snrproject/LoginActivity.kt
+++ app/src/main/java/com/example/snrproject/LoginActivity.kt
@@ -3,9 +3,16 @@ package com.example.snrproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.view.Gravity
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import coil.api.load
 import kotlinx.android.synthetic.main.activity_login_page.*
 
 class LoginActivity : AppCompatActivity() {
+
+    internal var dbUsers = DatabaseHelper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +25,22 @@ class LoginActivity : AppCompatActivity() {
         }
 
         loginBtn.setOnClickListener(){
-            val intent = Intent(this, ListPics::class.java)
-            intent.putExtra("username",login_username_edittext.text.toString())
-            startActivity(intent)
+            val dbUsers = dbUsers.allData
+            for (i in dbUsers.indices) {
+
+                //filter by profile
+                if(dbUsers[i].userName==login_username_edittext.text.toString() && dbUsers[i].userPass==login_password_edittext.text.toString()) {
+                    val intent = Intent(this, ListPics::class.java)
+                    intent.putExtra("username",login_username_edittext.text.toString())
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this@LoginActivity, "INCORRECT LOGIN CREDENTIALS", Toast.LENGTH_LONG).show()
+                    login_password_edittext.setText("")
+                    login_username_edittext.setText("")
+                }
+            }
+
         }
     }
 
