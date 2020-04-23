128e8e8f6cb9cd248f8563ef27212daa1df96364
MattAko <mattako66@gmail.com>
2020-04-23 16:30:01 -0700

    Upload new url to user database within the profile page
    *BUGS*
    The current DatabaseHelper does not update the URL properly for the user.

--- app/src/main/java/com/example/snrproject/Users.kt
+++ app/src/main/java/com/example/snrproject/Users.kt
@@ -1,4 +1,4 @@
 package com.example.snrproject
 
-class Users (var userID:String, var userName:String, var userPass:String, var userEmail:String, var userUrl:String) {
+class Users (var userID:String, var userName:String, var userPass:String, var userEmail:String, var userURL:String) {
 }
