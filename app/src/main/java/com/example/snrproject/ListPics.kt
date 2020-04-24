c98bc8da2cfe7fd4fec74b27ed46f2c322b0c1c4
Vector Le <vectorle@protonmail.com>
2020-04-23 21:37:50 -0700

    added ability to use map

--- app/src/main/java/com/example/snrproject/ListPics.kt
+++ app/src/main/java/com/example/snrproject/ListPics.kt
@@ -11,16 +11,24 @@ import android.content.Intent
 import android.util.Log
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_login_page.*
-
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 class ListPics : AppCompatActivity() {
     private var dbImages = ImageDatabase(this)
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap : GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val username = intent.getStringExtra("username")
         setContentView(R.layout.activity_list_pics)
         setupButtons(username)
+        loadMap()
         loadImages()
     }
 
@@ -43,8 +51,18 @@ class ListPics : AppCompatActivity() {
             intent.putExtra("username",username)
             startActivity(intent)
         }
-
     }
+
+    // load map
+    private fun loadMap() {
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+
+            googleMap.isMyLocationEnabled = true
+        })
+    }
+
     //load images from database
     private fun loadImages(){
         /*
@@ -82,6 +100,12 @@ class ListPics : AppCompatActivity() {
             layout.addView(loc)
             layout.addView(image)
             layout.addView(caption)
+
+            // get pins
+            var coordinates = res[i].userLocation.split(", ")
+            var location = LatLng(coordinates[0].toDouble(), coordinates[1].toDouble()) // create coordinate
+//            googleMap.addMarker(MarkerOptions().position(location)) // add pin to location
+//            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 15f)) // zoom map to general area
         }
     }
 }
